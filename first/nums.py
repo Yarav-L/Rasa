3c1c2cacc213becf97c44a5404648c7b0c4f8de4
lvm19 <lvm1998@yandex.ru>
2021-11-23 13:34:28 +0300

    add module string.py, text delete and add function multi

--- first/nums.py
+++ first/nums.py
@@ -1,12 +1,15 @@
 def plus(a, b):
-    return a+b
+    return a + b
 
-text = 'new text'
 
-def minus(a,b):
-    return a-b
+def minus(a, b):
+    return a - b
 
-print(minus(5,2))
+
+def multi(a, b):
+    return a * b
+
+
+print(multi(10, 10))
+print(minus(5, 2))
 print(plus(2, 2))
-
-
